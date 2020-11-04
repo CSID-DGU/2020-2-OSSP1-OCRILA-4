424741a9293bac1b504d50d834bb735e796ac7db
sujin kim <hidong1015@dgu.ac.kr>
2020-11-05 01:39:18 +0900

    tab 1,2,3 commit

--- myapplication/src/main/java/com/example/myapplication/MainActivity.java
+++ myapplication/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.myapplication;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager vp = findViewById(R.id.viewpager);
+        VPAdapter adapter = new VPAdapter(getSupportFragmentManager());
+        vp.setAdapter(adapter);
+
+        // 연동
+        TabLayout tab = findViewById(R.id.tab);
+        tab.setupWithViewPager(vp);
+
+
     }
 }
