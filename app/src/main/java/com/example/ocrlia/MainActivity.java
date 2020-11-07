08568c7a24fc89222d9f0727b17e5e4e15282a6e
용재 권 <dydwo2324@dgu.ac.kr>
2020-11-08 01:31:04 +0900

    init

--- app/src/main/java/com/example/ocrlia/MainActivity.java
+++ app/src/main/java/com/example/ocrlia/MainActivity.java
@@ -1,21 +1,32 @@
 package com.example.ocrlia;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
+
 import com.google.android.material.tabs.TabLayout;
 
 import androidx.viewpager.widget.ViewPager;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.view.Menu;
-import android.view.MenuItem;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.ocrlia.ui.main.SectionsPagerAdapter;
-public class MainActivity extends AppCompatActivity {
 
+import java.io.InputStream;
+
+public class MainActivity extends AppCompatActivity {
+    static final int REQUEST_CODE = 1;
+    Button camerabtn = null;
+    Button gallerybtn = null;
+    ImageView imageView =null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,7 +36,40 @@ public class MainActivity extends AppCompatActivity {
         viewPager.setAdapter(sectionsPagerAdapter);
         TabLayout tabs = findViewById(R.id.tabs);
         tabs.setupWithViewPager(viewPager);
+        camerabtn = (Button)findViewById(R.id.openCamera);
+        gallerybtn = (Button)findViewById(R.id.openGallery);
+        imageView=(ImageView)findViewById(R.id.imageView);
+        }
+    public void openCamera(View view){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(intent);
+    }
+    public void openGallery(View view){
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent,REQUEST_CODE);
+        startActivity(intent);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            if (resultCode == RESULT_OK) {
+                try {
+                    InputStream in = getContentResolver().openInputStream(data.getData());
 
+                    Bitmap img = BitmapFactory.decodeStream(in);
+                    in.close();
+
+                    imageView.setImageBitmap(img);
+                } catch (Exception e) {
+
+                }
+            } else if (resultCode == RESULT_CANCELED) {
+                Toast.makeText(this, "사진 선택 취소", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
