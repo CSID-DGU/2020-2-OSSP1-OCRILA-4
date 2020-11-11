4b65a546505b554269608917101402c1785bcb1e
LuneRemer <europejumin@gmail.com>
2020-11-11 14:31:55 +0900

    Camera and Crop Connect & Gallery and Crop Connect. In the middle of Process of saving the cropped image in Dir.

--- test/src/main/java/com/theartofdev/edmodo/cropper/test/MainActivity.java
+++ test/src/main/java/com/theartofdev/edmodo/cropper/test/MainActivity.java
@@ -1,8 +1,16 @@
 package com.theartofdev.edmodo.cropper.test;
 
+import android.Manifest;
 import android.content.Intent;
+import android.media.Image;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Environment;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -11,7 +19,16 @@ import com.example.test.R;
 import com.theartofdev.edmodo.cropper.CropImage;
 import com.theartofdev.edmodo.cropper.CropImageView;
 
+import java.io.File;
+import java.io.IOException;
+
 public class MainActivity extends AppCompatActivity {
+
+  static final int REQUEST_TAKE_PHOTO = 308;
+  static final int REQUEST_SAVE_IMAGE = 213;
+  String mCurrentPhotoPath;
+  boolean album = false;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -20,13 +37,36 @@ public class MainActivity extends AppCompatActivity {
   }
 
   /** Start pick image activity with chooser. */
-  public void onSelectImageClick(View view) {
+  public void onSelectImageClick_camera(View view) {
     CropImage.activity(null).setGuidelines(CropImageView.Guidelines.ON).start(this);
   }
 
+  public void onSelectImageClick_gallery(View view) {
+    pickFromGallery();
+  }
+
+  private void pickFromGallery() {
+    Intent intent = new Intent(Intent.ACTION_GET_CONTENT)
+            .setType("image/*")
+            .addCategory(Intent.CATEGORY_OPENABLE);
+
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+      String[] mimeTypes = {"image/jpeg", "image/png"};
+      intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes);
+    }
+    startActivityForResult(Intent.createChooser(intent,"Select Picture"), REQUEST_TAKE_PHOTO);
+  }
+
+  private File makeStorageFolder() throws IOException {
+    String imageFileName = "tmp_"+String.valueOf(System.currentTimeMillis())+".jpg";
+    File storageDir = new File(Environment.getExternalStorageDirectory(),imageFileName);
+    mCurrentPhotoPath = storageDir.getAbsolutePath();
+    return storageDir;
+  }
+
+  @RequiresApi(api = Build.VERSION_CODES.M)
   @Override
   protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
     // handle result of CropImageActivity
     if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE) {
       CropImage.ActivityResult result = CropImage.getActivityResult(data);
@@ -37,6 +77,12 @@ public class MainActivity extends AppCompatActivity {
             .show();
       } else if (resultCode == CropImage.CROP_IMAGE_ACTIVITY_RESULT_ERROR_CODE) {
         Toast.makeText(this, "Cropping failed: " + result.getError(), Toast.LENGTH_LONG).show();
+      }
+    }
+    if (requestCode == REQUEST_TAKE_PHOTO) {
+      final Uri selectedUri = data.getData();
+      if(selectedUri!=null){
+        CropImage.activity(selectedUri).setGuidelines(CropImageView.Guidelines.ON).start(this);
       }
     }
   }
