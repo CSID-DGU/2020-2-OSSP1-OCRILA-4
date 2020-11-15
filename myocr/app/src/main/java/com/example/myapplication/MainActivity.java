cfafc205006584d4c34c157af705e6b756767517
dndkwk <rmswn3147@gmail.com>
2020-11-15 18:01:03 +0900

    cropper 구현

---
+++ myocr/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,119 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import com.yalantis.ucrop.UCrop;
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView img;
+    private Button btn;
+    private final int CODE_IMG_GALLERY=1;
+    private final String SAMPLE_CROPPED_IMG_NAME="SampleCrop";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        img=(ImageView)findViewById(R.id.imageView2);
+        btn=(Button)findViewById(R.id.button);
+        //init();
+
+        btn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent().
+                        setAction(Intent.ACTION_GET_CONTENT)
+                        .setType("image/*"),CODE_IMG_GALLERY);
+            }
+        });
+
+        /*
+        img.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                startActivityForResult(new Intent().
+                        setAction(Intent.ACTION_GET_CONTENT)
+                        .setType("image/*"),CODE_IMG_GALLERY);
+            }
+        });
+        */
+
+    }
+
+    private void init(){
+        this.img=findViewById(R.id.imageView2);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == CODE_IMG_GALLERY && resultCode==RESULT_OK){
+            //DESDE GALLERY
+            Uri imageUri=data.getData();
+            if(imageUri!=null){
+                startCrop(imageUri);
+            }
+
+        }else if(requestCode == UCrop.REQUEST_CROP && resultCode == RESULT_OK){
+            Uri imageUriResultCrop = UCrop.getOutput(data);
+
+            if(imageUriResultCrop!=null){
+                img.setImageURI(imageUriResultCrop);
+            }
+        }
+    }
+
+    private void startCrop(@NonNull Uri uri){
+        String destinationFileName = SAMPLE_CROPPED_IMG_NAME;
+        destinationFileName +=".jpg";
+
+        UCrop uCrop=UCrop.of(uri,Uri.fromFile(new File(getCacheDir(),destinationFileName)));
+
+        //uCrop.withAspectRatio(1,1);
+        uCrop.withAspectRatio(3,4);
+        //uCrop.useSourceImageAspectRatio()
+        //uCrop.withAspectRatio(2,3);
+        //uCrop.withAspectRatio(16,5);
+
+        uCrop.withMaxResultSize(450,450);
+
+        uCrop.withOptions(getCropOption());
+
+        uCrop.start(MainActivity.this);
+    }
+
+    private UCrop.Options getCropOption(){
+        UCrop.Options options=new UCrop.Options();
+
+        options.setCompressionQuality(70);
+
+        //CompressType
+        //options.setCompressionFormat(Bitmap.CompressFormat.PNG);
+        //options.setCompressionFormat(Bitmap.CompressFormat.JPEG);
+
+        //UI
+        options.setHideBottomControls(false);
+        options.setFreeStyleCropEnabled(true);
+
+        //Colors
+        options.setStatusBarColor(getResources().getColor(R.color.design_default_color_primary_dark));
+        options.setToolbarColor(getResources().getColor(R.color.design_default_color_primary));
+
+        options.setToolbarTitle("Cropper");
+
+        return options;
+    }
+
+}
