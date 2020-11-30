280551ed2b820ea01440031e5e3c5e9049536526
admin1 <leski@naver.com>
2020-11-30 17:29:35 +0900

    11월 30일 OCR+DB 통합본 commit

---
+++ 8.11월30일DB통합본/2020-2-OSSP1-OCRILA-4-test/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.myapplication;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //Toolbar toolbar = findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, FilterActivity.class);
+                startActivityForResult(intent, 101);
+            }
+        });
+
+        Button camera_button = findViewById(R.id.camera_button);
+        camera_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (getApplicationContext(),CameraActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button gallery_button = findViewById(R.id.gallery_button);
+        gallery_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (getApplicationContext(),GalleryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+        // SharedPreferences 설정을 통한, DB insert문 실행
+        AppPreference appPreference = new AppPreference(MainActivity.this);
+
+        Boolean firstRun = appPreference.getFirstRun();
+        if(firstRun) { // 첫실행이면 아래 코드 실행
+            DatabaseUse dbUse = new DatabaseUse();
+            dbUse.firstInsert(getApplicationContext());
+
+            appPreference.setFirstRun(false); // false로 바꿈
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+
+}
